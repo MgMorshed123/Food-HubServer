c0152962806c54fc95a730288700f51b3ec1fec0
mdarf <mdarfinji45@gmail.com>
2024-12-09 02:29:58 +0800

    send welcomeemail addded

--- mailtrap/email.ts
+++ mailtrap/email.ts
@@ -18,3 +18,23 @@ export const sendVerificationEmail = async (
     throw new Error("Failed to send email verification");
   }
 };
+
+export const sendWelcomeEmail = async (email: string, name: string) => {
+  const recipient = [{ email }];
+  const htmlContent = generateWelcomeEmailHtml(name);
+  try {
+    const res = await client.send({
+      from: sender,
+      to: recipient,
+      subject: "Welcome to PatelEats",
+      html: htmlContent,
+      template_variables: {
+        company_info_name: "PatelEats",
+        name: name,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to send welcome email");
+  }
+};
